60a55a52690b0a64fd91a964170ed0134f468ba3
Ritika shrestha <87307821+ritsth@users.noreply.github.com>
2021-10-25 20:46:17 +0545

    Update settings.py

--- branchxpp/settings.py
+++ branchxpp/settings.py
@@ -188,7 +188,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 from datetime import timedelta
 AWS_S3_SIGNATURE_VERSION = "s3v4"
-AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_REGION_NAME = "us-east-2"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
